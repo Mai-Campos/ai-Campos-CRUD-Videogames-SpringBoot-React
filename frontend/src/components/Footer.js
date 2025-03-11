63e4b53aea59f54524cdb693471eaffc63f808d8
Maikol  Campos <Camposmaikol1@gmail.com>
2025-03-11 04:54:01 -0400

    Added components for header, footer and list to display video games~

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+// src/components/Footer.js
+import React from 'react';
+import './Footer.css'; 
+
+const Footer = () => {
+    return (
+        <footer className="bg-dark text-white text-center py-3 footer">
+            <div className="container">
+                <p className="mb-0">CRUD for videogame management developed by Maikol Campos Rodriguez</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <nav class="navbar navbar-expand-lg ">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <h1 className='title'>Life is a Game</h1></a>
+            <div className='navbar-icons'>
+                <img className="icon-navbar" width="64" height="64" src="https://img.icons8.com/external-photo3ideastudio-lineal-color-photo3ideastudio/64/external-game-pad-home-activity-photo3ideastudio-lineal-color-photo3ideastudio.png" alt="Game Pad" />
+                <img className="icon-navbar" width="48" height="48" src="https://img.icons8.com/doodle/48/super-mario.png" alt="Super Mario" />
+                <img className="icon-navbar" width="50" height="50" src="https://img.icons8.com/color/50/skyrim.png" alt="skyrim"/> 
+            </div>
+        </div>
+      </nav>
+    );
+};
+
+export default Header;
